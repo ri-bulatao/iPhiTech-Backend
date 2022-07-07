dfc4e1cafea1f0c8071d5a9ece4a3698812ba430
bestworks221 <best.works0012@gmail.com>
2022-07-07 11:08:01 +0800

    Added announcement store function

---
+++ app/Http/Requests/Announcement/AnnouncementRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Announcement;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Announcement as AnnouncementModel;
+use App\Models\Role;
+use Auth;
+
+class AnnouncementRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|unique:announcements',
+            'excerpt'   => 'required',
+            'content'  => 'required|min:8',
+            'receiver'  => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required'    => 'Title is required',
+            'title.unique'      => 'Title already taken',
+            'excerpt.required'  => 'Excerpt is required',
+            'content.required'  => 'Content are required',
+            'receiver.required' => 'Receiver is required'
+        ];
+    }
+}
