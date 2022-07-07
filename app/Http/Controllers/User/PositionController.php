a09b0c21a0805b9f10844152a330ca17cdfcb717
bestworks221 <best.works0012@gmail.com>
2022-07-07 11:17:01 +0800

    clean the controller and updated the user controller

--- app/Http/Controllers/User/PositionController.php
+++ app/Http/Controllers/User/PositionController.php
@@ -1,24 +1,26 @@
 <?php
 
-namespace App\Http\Controllers;
+declare(strict_types=1);
 
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Position;
 
 class PositionController extends Controller
 {
     public function index() {
-        // $positions = Position::all()->toArray();
-        // return array_reverse($positions);
-
-        return response()->json([
-            'positons' => 'test'
-        ]);
+        $positions = Position::all()->toArray();
+        return array_reverse($positions);
     }
     public function store(Request $request) {
         $position = new Position(['name' => $request->input('name')]);
         $position->save();
-        return response()->json('Position created!');
+        return response()->json([
+            "success" => true,
+            "message" => "Position created!"
+        ]);
     }
     public function show($id) {
         $position = Position::find($id);
@@ -27,11 +29,17 @@ class PositionController extends Controller
     public function update($id, Request $request) {
         $position = Position::find($id);
         $position->update($request->all());
-        return response()->json('Position updated!');
+        return response()->json([
+            "success" => true,
+            "message" => "Position updated!"
+        ]);
     }
     public function destroy($id) {
         $position = Position::find($id);
         $position->delete();
-        return response()->json('Position deleted!');
+        return response()->json([
+            "success" => true,
+            "message" => "Position deleted!"
+        ]);
     }
 }
