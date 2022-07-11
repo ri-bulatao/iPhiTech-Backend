0c159e77d76ab60b5c6eb622ecb6548692e361b2
bestworks221 <best.works0012@gmail.com>
2022-07-11 14:41:24 +0800

    Added pusher client

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -13,30 +13,33 @@
         <ul class="navbar-nav">
           <locale-dropdown />
           <!-- <li class="nav-item">
-            <router-link :to="{ name: 'admin.announcements.list' }" class="nav-link">
-              Announcements
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'admin.positions.list' }" class="nav-link">
-              Positions
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'admin.users.list' }" class="nav-link">
-              Users
-            </router-link>
+            <a class="nav-link" href="#">Link</a>
           </li> -->
         </ul>
 
         <ul class="navbar-nav ms-auto">
+
+          <li v-if="user" class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle btn btn-info"
+               href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+            >
+              Notifications
+              <span class="badge badge-pill badge-light">{{ notifications.length }}</span>
+            </a>
+            <div class="dropdown-menu">
+              <a href="javascript:void(0)" @click="() => updateRedirect(notification)" class="dropdown-item ps-3" v-for="notification in notifications" :key="notification.id">{{ notification.title }}</a>
+              <div class="dropdown-divider" />
+              <router-link :to="{ name: 'front.notifications' }" class="dropdown-item ps-3">See all Notifications</router-link>
+            </div>
+          </li>
+
           <!-- Authenticated -->
           <li v-if="user" class="nav-item dropdown">
             <a class="nav-link dropdown-toggle text-dark"
                href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
             >
               <img :src="user.photo_url" class="rounded-circle profile-photo me-1">
-              {{ user.full_name }}
+              {{ user.name }}
             </a>
             <div class="dropdown-menu">
               <router-link :to="{ name: 'settings.profile' }" class="dropdown-item ps-3">
@@ -71,8 +74,10 @@
 </template>
 
 <script>
+import Swal from 'sweetalert2'
 import { mapGetters } from 'vuex'
 import LocaleDropdown from './LocaleDropdown'
+import Pusher from 'pusher-js'
 
 export default {
   components: {
@@ -84,7 +89,8 @@ export default {
   }),
 
   computed: mapGetters({
-    user: 'auth/user'
+    user: 'auth/user',
+    notifications: 'notifications/unreadNotifications'
   }),
 
   methods: {
@@ -94,7 +100,45 @@ export default {
 
       // Redirect to login.
       this.$router.push({ name: 'login' })
+    },
+
+    updateRedirect(notification) {
+      let payload = {
+        id: notification.id,
+        title: notification.title,
+        url: notification.url,
+        read: ! notification.read
+      }
+
+      this.$store.dispatch('notifications/updateNotification', payload)
+        .then(res => {
+          this.$store.dispatch('notifications/fetchUnreadNotifications')
+          this.$router.push(notification.url)
+        })
+        .catch(err => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Something went wrong!'
+          })
+        })
     }
+  },
+
+  created() {
+    let pusher = new Pusher('3ac02751a4aa9ae8d60c', {
+      cluster: 'ap1'
+    })
+
+    let channel = pusher.subscribe('posted-announcements');
+
+    channel.bind('posted-announcements', (data) => {
+      alert('Announcement Posted')
+      console.log(data)
+    })
+  },
+
+  mounted() {
+    this.$store.dispatch('notifications/fetchUnreadNotifications')
   }
 }
 </script>
