e32ae57d7c2af9607fe5772af4044c6c88267c19
bestworks221 <bestworks221@users.noreply.github.com>
2022-07-12 02:28:25 +0000

    :hammer: Fix PHP styling based on configured rules :heavy_check_mark:

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -31,7 +31,7 @@ class UserController extends Controller
 
     /**
      * For Fetching all users.
-     * Excep the Admin user
+     * Excep the Admin user.
      */
     public function index(): JsonResponse
     {
@@ -61,7 +61,7 @@ class UserController extends Controller
     public function store(UserRequest $request): JsonResponse
     {
 
-        # Check if admin trying to change the password
+        // Check if admin trying to change the password
         $new_password = '';
         if ($request->new_password != '') {
             // Encrypt the password
@@ -102,7 +102,7 @@ class UserController extends Controller
 
         $userAddress = UserAddressModel::create($addressData);
 
-        if ( $user && $userAddress) {
+        if ($user && $userAddress) {
             return $this->result->created($user, __('messages.user_create_response'));
         }
 
@@ -116,7 +116,7 @@ class UserController extends Controller
     {
         $user = UserModel::find($id);
 
-        if ( !$user ) {
+        if (! $user) {
             return $this->result->notFound();
         }
 
@@ -126,33 +126,33 @@ class UserController extends Controller
             'phone_number'  =>  $request->ec_phone_number,
         ];
 
-        # Check if admin trying to change the password
+        // Check if admin trying to change the password
         if ($request->new_password != '') {
             // encrypt the password
             $newPassword = bcrypt($request->new_password);
             $user->password = $newPassword;
         }
 
-        $user->first_name           = $request->first_name;
-        $user->middle_name          = $request->middle_name;
-        $user->last_name            = $request->last_name;
-        $user->phone_number         = $request->phone_number;
-        $user->email                = $request->email;
-        $user->gender               = $request->gender;
-        $user->marital_status       = $request->marital_status;
-        $user->date_of_birth        = $request->date_of_birth;
-        $user->emergency_contact    = json_encode($emergencyContact);
-        $user->position_id          = $request->position;
+        $user->first_name = $request->first_name;
+        $user->middle_name = $request->middle_name;
+        $user->last_name = $request->last_name;
+        $user->phone_number = $request->phone_number;
+        $user->email = $request->email;
+        $user->gender = $request->gender;
+        $user->marital_status = $request->marital_status;
+        $user->date_of_birth = $request->date_of_birth;
+        $user->emergency_contact = json_encode($emergencyContact);
+        $user->position_id = $request->position;
         $user->save();
 
-        # Update User Address Model
+        // Update User Address Model
         $userAddress = UserAddressModel::where('user_id', $user->id)->first();
-        if ( !empty($userAddress) ) {
-            $userAddress->street_address    = $request->street_address;
-            $userAddress->city              = $request->city;
-            $userAddress->state             = $request->state;
-            $userAddress->zip_code          = $request->zip_code;
-            $userAddress->country           = $request->country;
+        if (! empty($userAddress)) {
+            $userAddress->street_address = $request->street_address;
+            $userAddress->city = $request->city;
+            $userAddress->state = $request->state;
+            $userAddress->zip_code = $request->zip_code;
+            $userAddress->country = $request->country;
             $userAddress->save();
         } else {
             // user address
