11f3c25c93567adfbe410794c2ad606fded20374
bestworks221 <best.works0012@gmail.com>
2022-07-19 10:07:45 +0800

    minor fixes on layout for better view

--- resources/js/pages/admin/position/AdminUserPosition.vue
+++ resources/js/pages/admin/position/AdminUserPosition.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid custom-container">
         <!-- Header -->
         <div class="row">
-            <div class="col-sm-12 col-lg-7 m-auto d-flex justify-content-between align-items-center">
+            <div class="col-sm-12 col-lg-12 d-flex justify-content-between align-items-center">
                 <h1>Positions</h1>
             </div>
         </div>
         
         <!-- Contents -->
         <div class="row">
-            <div class="col-sm-12 col-lg-7 m-auto">
+            <div class="col-sm-12 col-lg-12">
                 <router-view></router-view>
             </div>
         </div>
